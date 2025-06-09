87fffc4173dbb80c850ac4edd303810601c1160d
MuhammadRioAprilianto <rma994636@gmail.com>
2025-06-09 14:31:59 +0700

    unlimited loop untuk menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main(){
     string baris; //membuat variable baring
     ofstream outfile; //membuat objek output file stream
-    outfile.open("contohfile.txt") //membuka file yang telah dibuat untuk ditulisi
+    outfile.open("contohfile.txt"); //membuka file yang telah dibuat untuk ditulisi
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+
+    }
 }
